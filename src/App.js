8f3928281dd8a4d5115f2cf368fcae7fbce4bfc6
do.falamin@mail.ru <solburner@outlook.com>
2024-07-17 00:26:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   console.log(window.Telegram.WebApp.initData);
 
-  return <div className="App">Awesome!</div>;
+  return <div className="App">Awesome!!!Awesome</div>;
 }
 
 export default App;
