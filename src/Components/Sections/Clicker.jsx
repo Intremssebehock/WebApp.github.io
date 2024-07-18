c7996d1ae51ccd04b191fa1460c3104de13e301f
do.falamin@mail.ru <solburner@outlook.com>
2024-07-18 11:13:40 +0300

    5

--- src/Components/Sections/Clicker.jsx
+++ src/Components/Sections/Clicker.jsx
@@ -3,51 +3,51 @@ import './Section.css';
 
 function Clicker() {
   const canvasRef = useRef(null);
-  const canvasWidth = 430;
-  const canvasHeight = 932;
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
-    console.log(canvas.style);
+    console.log(window.innerWidth + ' / ' + window.innerHeight);
+    console.log(canvas.width + ' / ' + canvas.height);
 
-    const drawStar = (cx, cy, spikes, outerRadius, innerRadius) => {
-      let rot = (Math.PI / 2) * 3;
-      let x = cx;
-      let y = cy;
-      const step = Math.PI / spikes;
-
-      ctx.strokeStyle = '#000';
-      ctx.beginPath();
-      ctx.moveTo(cx, cy - outerRadius);
-      for (let i = 0; i < spikes; i++) {
-        x = cx + Math.cos(rot) * outerRadius;
-        y = cy + Math.sin(rot) * outerRadius;
-        ctx.lineTo(x, y);
-        rot += step;
-
-        x = cx + Math.cos(rot) * innerRadius;
-        y = cy + Math.sin(rot) * innerRadius;
-        ctx.lineTo(x, y);
-        rot += step;
-      }
-      ctx.lineTo(cx, cy - outerRadius);
-      ctx.closePath();
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = '#EDC70C';
-      ctx.stroke();
-      ctx.fillStyle = '#B59912';
-      ctx.fill();
+    const resizeCanvas = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
+    resizeCanvas();
 
-    // Рисуем несколько звезд с разными параметрами
-    drawStar(canvasWidth / 2, canvasHeight / 2 - 100, 10, 60, 150);
-  }, []);
+    window.addEventListener('resize', resizeCanvas);
 
-  const drawNewStar = (cx, cy, spikes, outerRadius, innerRadius) => {
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, [window.innerWidth]);
+
+  useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    canvas.width = dimensions.width;
+    canvas.height = dimensions.height;
+    // Очистка canvas перед рисованием новой звезды
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Координаты центра canvas
+    const centerX = dimensions.width / 2;
+    const centerY = dimensions.height / 2;
+
+    // Рисуем звезду в центре
+    drawStar(ctx, centerX, centerY, 20, 30, 25);
+  }, [dimensions]);
+
+  // Рисование звезды
+  const drawStar = (ctx, cx, cy, spikes, outerRadius, innerRadius) => {
     let rot = (Math.PI / 2) * 3;
     let x = cx;
     let y = cy;
@@ -70,18 +70,15 @@ function Clicker() {
     ctx.lineTo(cx, cy - outerRadius);
     ctx.closePath();
     ctx.lineWidth = 5;
+    ctx.strokeStyle = 'blue';
     ctx.stroke();
-    ctx.fillStyle = '#B59912';
+    ctx.fillStyle = 'skyblue';
+    ctx.fill();
   };
 
   return (
     <div className="Clicker-container">
-      <canvas
-        onClick={() => drawNewStar(canvasWidth / 2, canvasHeight / 2 - 50, 10, 90, 190)}
-        height="932px"
-        width="430px"
-        ref={canvasRef}
-        className="Clicker-canvas"></canvas>
+      <canvas ref={canvasRef} className="Clicker-canvas"></canvas>
     </div>
   );
 }
