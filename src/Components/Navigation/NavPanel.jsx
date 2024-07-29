05ee237c6d7b74b32ad6a53135685447136f6a51
do.falamin@mail.ru <solburner@outlook.com>
2024-07-30 01:16:45 +0300

    6

--- src/Components/Navigation/NavPanel.jsx
+++ src/Components/Navigation/NavPanel.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavPanel.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentPage } from '../../Redux/Slices/PageSlice';
 
 function NavPanel() {
+  const currentPage = useSelector((state) => state.page.currentPage);
+  const dispatch = useDispatch();
+
   return (
     <div className="nav-panel">
       <nav className="nav-panel-container">
         <ul className="link-list">
-          <li className="clicker-link link">
+          <li
+            onClick={() => dispatch(setCurrentPage('/'))}
+            className={`clicker-link link ${currentPage === '/' ? 'active' : ''}`}>
             <Link className="link-batton" to="/"></Link>
           </li>
-          <li className="shop-link link">
+          <li
+            onClick={() => dispatch(setCurrentPage('/shop'))}
+            className={`shop-link link ${currentPage === '/shop' ? 'active' : ''}`}>
             <Link className="link-batton" to="/shop"></Link>
           </li>
-          <li className="rating-link link">
+          <li
+            onClick={() => dispatch(setCurrentPage('/rating'))}
+            className={`rating-link link ${currentPage === '/rating' ? 'active' : ''}`}>
             <Link className="link-batton" to="/rating"></Link>
           </li>
-          <li className="invitations-link link">
+          <li
+            onClick={() => dispatch(setCurrentPage('/invitations'))}
+            className={`invitations-link link ${currentPage === '/invitations' ? 'active' : ''}`}>
             <Link className="link-batton" to="/invitations"></Link>
           </li>
         </ul>
--- src/Components/Sections/Shop.jsx
+++ src/Components/Sections/Shop.jsx
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SnakePreview from './../../images/SnakePreview.jpg';
 
 function Shop() {
-  return <div>123</div>;
+  const [activeSection, setActiveSection] = useState('auto'); // Изначально выбран раздел "Auto"
+  const [games, setGames] = useState([{ name: 'Змейка', price: 'Бесплатно', img: SnakePreview }]);
+
+  // Функция для переключения разделов
+  const handleSectionChange = (section) => {
+    setActiveSection(section);
+  };
+
+  return (
+    <div className="store-container">
+      <h1 className="store-title">Improve</h1>
+      <div className="store-buttons">
+        <button
+          className={`store-button ${activeSection === 'auto' ? 'active' : ''}`}
+          onClick={() => handleSectionChange('auto')}>
+          +Auto
+        </button>
+        <button
+          className={`store-button ${activeSection === 'click' ? 'active' : ''}`}
+          onClick={() => handleSectionChange('click')}>
+          +Click
+        </button>
+        <button
+          className={`store-button ${activeSection === 'games' ? 'active' : ''}`}
+          onClick={() => handleSectionChange('games')}>
+          +Games
+        </button>
+      </div>
+      <div className="store-content">
+        {activeSection === 'auto' && (
+          <div className="store-section">
+            <p className="section-title">Labor exchange</p>
+            <p className="section-description">
+              Нанимайте работников, которые будут кликать за вас и приносить автоматический доход
+            </p>
+          </div>
+        )}
+        {activeSection === 'click' && (
+          <div className="store-section">
+            <p className="section-title">Finger Swing</p>
+            <p className="section-description">
+              Делайте упражнения на пальцы и получайте больше от одного касания{' '}
+            </p>
+          </div>
+        )}
+        {activeSection === 'games' && (
+          <div className="store-section">
+            <p className="section-title">Mini-games</p>
+            <p className="section-description">
+              Покупайте новые мини-игры и получайте больше иксов. Игры часто появляются в пузырях,
+              когда вы кликаете монету
+            </p>
+            <ul className="game-list">
+              {games.map((game, index) => (
+                <li className="game-list-item">
+                  <div className="game-container">
+                    <div className="game-info">
+                      <p className="title">{game.name}</p>
+                      <p className="price">{game.price}</p>
+                    </div>
+                    <img className="game-preview" src={game.img} alt="GamePreview" />
+                  </div>
+                  <button className="receive">Получить</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Shop;
